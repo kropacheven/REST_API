10771b6a355479d908e85c1648672750e43806e8
kropacheven <81347649+kropacheven@users.noreply.github.com>
2023-04-15 16:11:23 +0500

    Database connection authentication
    Set successfully

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 // load modules
 const express = require('express');
 const morgan = require('morgan');
+const {sequelize} = require('./models');
 
 // variable to enable global error logging
 const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
@@ -38,6 +39,19 @@ app.use((err, req, res, next) => {
     error: {},
   });
 });
+
+
+// Database connection test:
+(async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync({force: false});
+    console.log("Connection to database is set successfully!!!");
+  } 
+  catch (err) {
+    console.log("Connection to database is not successfull:(");
+  }
+}) ();
 
 // set our port
 app.set('port', process.env.PORT || 5000);
