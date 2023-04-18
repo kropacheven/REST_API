1de72a1da84d1a380d97468065bf6dad15f27bc9
kropacheven <81347649+kropacheven@users.noreply.github.com>
2023-04-18 11:42:37 +0500

    Set GET/POST routes for users

--- routes.js
+++ routes.js
@@ -18,12 +18,30 @@ function asyncHandler(cb) {
     }
   }
 
+
 // Route that returns a list of users.
 router.get('/users', asyncHandler(async (req, res) => {
     let users = await User.findAll();
-    res.json(users);
+    res.status(200).json(users);
   }));
 
+// Route that creates a new user.
+router.post('/users', asyncHandler(async (req, res) => {
+    try {
+      await User.create(req.body);
+      res.status(201).json({ "message": "Account successfully created!" });
+    } catch (error) {
+      console.log('ERROR: ', error.name);
+  
+    //   if (error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') {
+    //     const errors = error.errors.map(err => err.message);
+    //     res.status(400).json({ errors });   
+    //   } else {
+    //     throw error;
+    //   }
+    }
+  }));
+  
 
 
   module.exports = router;
