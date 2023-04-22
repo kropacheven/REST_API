03f6d854fbccd5298ca7852cc7321856823dadb8
kropacheven <81347649+kropacheven@users.noreply.github.com>
2023-04-22 17:24:05 +0500

    Work on PUT route (empty object)

--- routes.js
+++ routes.js
@@ -87,15 +87,14 @@ router.post('/courses', authenticateUser, asyncHandler(async (req, res) => {
     } catch (error) {
       console.log('ERROR: ', error.name);
 
-    if (error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') {
-      const errors = error.errors.map(err => err.message);
-      res.status(400).json({ errors });   
-    } else {
-      throw error;
-    }
-    }
+      if (error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') {
+        const errors = error.errors.map(err => err.message);
+        res.status(400).json({ errors });   
+      } else {
+        throw error;
+      }
 
-
+    }
   }));
 
 
